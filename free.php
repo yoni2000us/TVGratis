1c405e00ec3c0075d364ac40138cc8938eaff8fc
yoni2000us <yoni2000mx@hotmail.com>
2016-04-26 15:30:54 -0500

    Create free.php

---
+++ free.php
@@ -0,0 +1,531 @@
+# TVGratis#EXTM3U
+
+#EXTINF:-1,video rola
+rtmp://200.52.193.57:1935/live/unique/live_2938
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Azteca 13 HD
+http://aztecalive-lh.akamaihd.net/i/0oxnvzh8a_1@175671/index_3_av-b.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Azteca 7 HD
+http://iptv2016-001-site1.1tempurl.com/live/jdm/7.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE", Azteca 7
+http://38.104.228.230:1938/info7/info7_512/playlist.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Multimedios Monterrey
+http://multimedioshls-i.akamaihd.net/hls/live/223741/57828478001-3/master.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Canal 2 
+http://tvsawpdvr-lh.akamaihd.net/i/stch02wp_1@119660/index_1048_av-b.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Canal 2 HD
+http://iptv2016-001-site1.1tempurl.com/live/jdm/52.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Canal de las estrellas 
+http://hlslive.lcdn.une.net.co/v1/AUTH_HLSLIVE/CEST/tu1_1.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Telemar Campeche. Mex
+http://wms.tecnoxia.com:1935/8004/8004/playlist.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",8 TV 
+http://201.130.193.150:1935/codecsys/smil:ocho.smil/playlist.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Canal 5 
+http://o12teamus.tk:8081/jdmsociety/canal5/playlist.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Gala TV 
+http://iptv2016-001-site1.1tempurl.com/live/jdm/109.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE", CANAL ONCE
+http://www.oncetvmexicolive.tv:1935/livepkgr2/smil:int.smil/playlist.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",PROYECTO 40
+http://urtmpkal-f.akamaihd.net/i/05i15qzql_1@149710/master.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Capital 21
+http://187.237.253.94:8134/hls-live/livepkgr/_definst_/liveevent.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE", TV MAS
+http://50.7.98.234:1935/rtv/videortv//playlist.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",Televisión Mexiquense
+http://fss28.streamhoster.com:1935/lv_tvmexiquense/broadcast1/playlist.m3u8
+
+#EXTINF:-1 group-title="CANALES ABIERTOS LIVE",CANAL DE CONGRESO MEX
+http://dl.canaldelcongreso.gob.mx:1935/ccongreso/mp4:congreso2b.mp4/playlist.m3u8
+
+#EXTINF:-1,Canal Judicial HD
+http://dl-scjn.scjn.gob.mx/scjn/scjn.mp4/playlist.m3u8
+
+#EXTINF:-1,unimas
+https://video01-lax.streamup.com/app/unimas-1s-stream_aac/playlist.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",TELEMUNDO 39
+https://video01-lax.streamup.com/app/anahix3s-stream_aac/playlist.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",TELEMUNDO 39
+http://iphone-streaming.ustream.tv/uhls/22243668/streams/live/iphone/playlist.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",UNIVISION
+https://video01-lax.streamup.com/app/anahix1s-stream_aac/playlist.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",UNIVISION
+http://iphone-streaming.ustream.tv/uhls/22277462/streams/live/iphone/playlist.m3u8
+
+#EXTINF:1,MaxTV
+https://video01-lax.streamup.com/app/maxtv-huicungos-channel_aac/playlist.m3u8
+
+#EXTINF:1,Peliculas Vitato
+https://video01-lax.streamup.com/app/vitato_aac/playlist.m3u8
+
+#EXTINF:1,Peliculas Broadmovies
+https://video01-lax.streamup.com/app/broadmoviess-channel_aac/playlist.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",Mundo Max
+https://video01-lax.streamup.com/app/ana69s-channel_aac/playlist.m3u8
+
+#EXTINF:1,Caracol HD
+http://stream.ssh101.com:1935/live/todocanales1/chunklist.m3u8
+
+#EXTINF:1,RCN
+http://lv13msssopsccc.4cloud.co/livercn/AppSenalRcn/master.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",Rpc TV
+http://live.rpctv.com/rpctv/rpc.smil/chunklist_w1859188126_b896000_slesp.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",El pinguino TV
+http://streaming.elpinguino.com:1935/live/pinguinotv_360p/live.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",Tele UnoTV
+http://k3.usastreams.com:1935/tvuno1/tvuno1/playlist.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",America TV 
+http://o12teamus.tk:8081/jdmsociety/AmericaHD/index_2200_av-p.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",America peru
+http://ooyalahd2-f.akamaihd.net/i/atv02_delivery@25405/index_564_av-p.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",Antena 3 HD
+http://a3live-lh.akamaihd.net/i/antena3/antena3_1@35248/master.m3u8?
+
+#EXTINF:-1,Antena 3 Internacional 
+http://hlslive.lcdn.une.net.co/v1/AUTH_HLSLIVE/ANT3/tu1_1.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",A3S HD
+http://a3live-lh.akamaihd.net/i/a3shds/geoa3series_1@122775/master.m3u8?
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",MEGA
+http://a3live-lh.akamaihd.net/i/mghds/geomega_1@328914/master.m3u8?
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",Neox
+http://a3live-lh.akamaihd.net/i/nxhds/geoneox_1@35261/master.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",Nova
+http://a3live-lh.akamaihd.net/i/nvhds/geonova_1@379404/master.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",Telecinco
+http://mdsios1-lh.akamaihd.net/i/mitele/esmediaset_11@168471/index_0_av-p.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",La Sexta HD
+http://a3live-lh.akamaihd.net/i/lasexta_1@35272/master.m3u8?
+
+#EXTINF:-1,la 2
+http://iphonelive.rtve.es/LA2_LV3_IPH/bitrate_2.m3u8
+
+#EXTINF:-1,TV 5 Monde
+http://hlslive.lcdn.une.net.co/v1/AUTH_HLSLIVE/TV5/tu1_manifest.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",MEGA Chile
+http://edge-41-us.edge.mdstrm.com/live/smil:db6a38a0a2bffd96d01965c4d975b9fb/playlist_b900.m3u8?
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",La Red
+http://wow1.lax.digitalproserver.com/redtv/redtvvivo/livestream1/hasbahca.m3u8?
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",TV Chile
+http://edge-61-cl.edge.mdstrm.com/tvn-live/_definst_/smil:285a5c6dade574d5f111419c85c6cf17/chunklist_b800000.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",TVN HD
+http://tvn-vod.mdstrm.com/liveedge/tvnelemental.smil/chunklist_b981000.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",FOX
+http://www.eliplextv.com/tv/demo.php?id=3947&f=.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",TBS 
+http://o12teamus.tk:8081/jdmsociety/tbs/playlist.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",DISTRITO COMEDIA
+http://o12teamus.tk:8081/jdmsociety/distritocomedia/playlist.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",TRUTV 
+http://104.236.98.15:42361/trutv/playlist.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",UNICABLE
+http://209.126.106.202:8080/unicable2/playlist.m3u8
+
+#EXTINF:-1 group-title="ENTRETENIMIENTO",FOX LIFE
+http://o12teamus.tk:8081/jdmsociety/foxlife/playlist.m3u8
+
+#EXTINF:-1 group-title="CINE",AZ CINEMA
+http://iptv2016-001-site1.1tempurl.com/live/jdm/425.m3u8
+
+#EXTINF:-1 group-title="CINE",UNIVERSAL HD
+http://iptv2016-001-site1.1tempurl.com/live/jdm/207.m3u8
+
+#EXTINF:-1 group-title="CINE",TNT Series
+http://o12teamus.tk:8081/jdmsociety/tntseries/playlist.m3u8
+
+#EXTINF:-1 group-title="CINE",TNT
+http://o12teamus.tk:8081/jdmsociety/tnt/playlist.m3u8
+
+#EXTINF:-1 group-title="CINE",DE PELICULA HD
+http://iptv2016-001-site1.1tempurl.com/live/jdm/442.m3u8
+
+#EXTINF:-1 group-title="CINE",STUDIO UNIVERSAL
+http://iptv2016-001-site1.1tempurl.com/live/jdm/492.m3u8
+
+#EXTINF:-1 group-title="CINE",GOLDEN HD
+http://iptv2016-001-site1.1tempurl.com/live/jdm/436.m3u8
+
+#EXTINF:-1 group-title="CINE",CINEMAX
+http://pungatv.xyz:666/live/yoni2000mx/34k3jh3g4k32g5f/22.m3u8
+
+#EXTINF:-1 group-title="CINE",CINECANAL
+http://209.126.106.202:8080/cinecanal/index.m3u8
+
+#EXTINF:-1 group-title="CINE",FILM ZONE 
+http://104.131.117.246:8081/chuy/76/033-FilmZone/playlist.m3u8
+
+#EXTINF:-1 group-title="CINE",GOLDEN 
+http://videos.visionlatina.tv:1935/LiveATV/!ji2/playlist.m3u8
+
+#EXTINF:-1 group-title="CINE",Fox Action
+http://iptv2016-001-site1.1tempurl.com/live/jdm/416.m3u8
+
+#EXTINF:-1 group-title="CINE",Fox Movies
+http://iptv2016-001-site1.1tempurl.com/live/jdm/417.m3u8
+
+#EXTINF:-1 group-title="CINE",Fox Classics
+http://o12teamus.tk:8081/jdmsociety/foxclassic/playlist.m3u8
+
+#EXTINF:-1 group-title="CINE",Fox Cinema
+http://104.236.94.5:42361/foxcinema/playlist.m3u8
+
+#EXTINF:-1 group-title="CINE",Fox 1
+http://104.236.94.15:42361/foxcinema/playlist.m3u8
+
+#EXTINF:-1 group-title="CINE",Fox Family
+http://o12teamus.tk:8081/jdmsociety/foxfamily/playlist.m3u8
+
+#EXTINF:-1,Paramount Channel
+http://www.eliplextv.com/tv/demo.php?id=1035&f=.m3u8
+
+#EXTINF:-1,cine dinamita
+http://www.eliplextv.com/tv/demo.php?id=3465&f=.m3u8
+
+#EXTINF:-1,cinelatino
+http://www.eliplextv.com/tv/demo.php?id=477&f=.m3u8
+
+#EXTINF:-1,hbo
+http://o12teamus.tk:8081/jdmsociety/hbo/playlist.m3u8
+
+#EXTINF:-1,hbo2
+http://o12teamus.tk:8081/jdmsociety/hbo2/playlist.m3u8
+
+#EXTINF:-1,hbo 2
+http://199.180.114.174:88/files/hbo222222.m3u8
+
+#EXTINF:1,Peliculas 
+http://iphone-streaming.ustream.tv/uhls/18654330/streams/live/iphone/playlist.m3u8?
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",LIFETIME
+http://iptv2016-001-site1.1tempurl.com/live/jdm/379.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",Home and Health HD
+http://o12teamus.tk:8081/jdmsociety/discoveryhh/playlist.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",History
+http://o12teamus.tk:8081/jdmsociety/history/playlist.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",H2
+http://o12teamus.tk:8081/jdmsociety/h2/playlist.m3u8
+
+#EXTINF:-1,H2
+http://www.eliplextv.com/tv/demo.php?id=297&f=.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",NAT GEO
+http://iptv2016-001-site1.1tempurl.com/live/jdm/377.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",NAT GEO WILD 
+http://videos.visionlatina.tv:1935/LiveATV/is!i2/playlist.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",ANIMAL PLANET
+http://o12teamnl.tk:8081/jdmsociety/animalplanet/playlist.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",DISCOVERY
+http://o12teamus.tk:8081/jdmsociety/discovery/playlist.m3u8
+
+#EXTINF:-1 tvg-logo="discovery channel.png",Discovery en Espanol
+http://iphone-streaming.ustream.tv/uhls/17248101/streams/live/iphone/playlist.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",TLC 
+http://o12teamus.tk:8081/jdmsociety/tlc/playlist.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",INVESTIGATION DISCOVERY
+http://o12teamus.tk:8081/jdmsociety/id/playlist.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",WOBI 
+http://o12teamus.tk:8081/jdmsociety/wobi/playlist.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",Fashion TV
+http://hls.novotelecom.ru/streaming/fashion/tvrec/playlist.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",Travel TV
+http://87.106.219.140:1935/live/myStream_2/playlist.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",Mexico Travel Channel
+http://cdns840stu1010.multistream.net/mtchlive/amlst:live/master.m3u8
+
+#EXTINF:-1 group-title="MUNDO & CULTURA",EWTN
+http://ewtn.mpl.miisolutions.net:1935/ewtn-live-5/900.mp4/playlist.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",HMX
+http://iptv2016-001-site1.1tempurl.com/live/jdm/169.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",ESPN HD 
+http://iptv2016-001-site1.1tempurl.com/live/jdm/516.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",ESPN
+http://www.eliplextv.com/tv/demo.php?id=2487&f=.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",ESPN2
+http://www.eliplextv.com/tv/demo.php?id=301&f=.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",ESPN 3
+http://www.eliplextv.com/tv/demo.php?id=3806&f=.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",FoxSports
+http://www.eliplextv.com/tv/demo.php?id=2505&f=.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",FoxSports 2
+http://www.eliplextv.com/tv/demo.php?id=475&f=.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",FoxSports 3
+http://www.eliplextv.com/tv/demo.php?id=445&f=.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",UFC Network
+http://www.eliplextv.com/tv/demo.php?id=9628&f=.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",BeinSports en Español
+http://www.eliplextv.com/tv/demo.php?id=2527&f=.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",Planeta Futbol
+http://www.eliplextv.com/tv/demo.php?id=1477&f=.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",TDN Mexico
+http://www.eliplextv.com/tv/demo.php?id=1059&f=.m3u8
+
+#EXTINF:-1,WinSports
+http://lcdn.une.net.co/v1/AUTH_HLSLIVE/0001/tu1/yt1_1.m3u8
+
+#EXTINF:-1,WinSports fut
+http://futbol.lcdn.une.net.co/v1/AUTH_HLSLIVE/0001/tu1/yt1_1.m3u8
+
+#EXTINF:-1,Univision TDN
+http://104.236.61.232:8081/live/sky69/UnivisionTDNcsp/playlist.m3u8
+
+#EXTINF:-1,TDN (eventos Online)
+http://tvsamontbk-lh.akamaihd.net/i/mtydepca02_1@120894/master.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",Real Madrid Channel
+http://rmtvlive-lh.akamaihd.net:80/i/rmtv_1@154306/master.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",Red Bull TV 
+http://sacapunta.eltelon.com/channels/redbulltv/playlist.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",Poker Central
+http://ooyalahd2-f.akamaihd.net/i/crtv01_delivery@329705/index_1164_av-b.m3u8
+
+#EXTINF:-1,F1
+http://104.131.94.43:1382/live/98/F1LAcsp/playlist.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",Eurosport 
+http://esioslive6-i.akamaihd.net/hls/live/202874/AL_P_ESP1_INTER_ENG/playlist_1000.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",TyC Sports
+http://live.vodgc.com:1935/kLive/smil:0_a5h483eu_all.smil/master.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",NBA
+http://208.92.36.158/ipad/live/nba_ipad_1240.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",NFL Network
+http://nflsvglagame1-i.akamaihd.net/hls/live/223206/NFL_Mobile/2015Mobile_NFLN_1000k.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",PAC-12 Net. (National)
+http://xrxs.net/video/live-p12netw-4728.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",PAC-12 Net. (Arizona)
+http://xrxs.net/video/live-p12ariz-4728.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",PAC-12 Net. (Bay Area)
+http://xrxs.net/video/live-p12baya-4728.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",PAC-12 Net. (Los Angeles)
+http://xrxs.net/video/live-p12losa-4728.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",PAC-12 Net. (Moutain)
+http://xrxs.net/video/live-p12moun-4728.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",PAC-12 Net. (Oregon)
+http://xrxs.net/video/live-p12oreg-4728.m3u8
+
+#EXTINF:-1 group-title="DEPORTES",PAC-12 Net. (Washington)
+http://xrxs.net/video/live-p12wash-4728.m3u8
+
+
+#EXTINF:-1 group-title="INFANTIL",BOOMERANG
+http://www.eliplextv.com/tv/demo.php?id=3947&f=.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",CARTOON NETWORK
+http://iptv2016-001-site1.1tempurl.com/live/jdm/320.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",DISNEY CHANNEL
+http://www.eliplextv.com/tv/demo.php?id=256&f=.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",DISNEY XD
+http://o12teamus.tk:8081/jdmsociety/disneyxd/playlist.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",DISNEY JUNIOR
+http://www.eliplextv.com/tv/demo.php?id=1455&f=.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",NICK
+http://www.eliplextv.com/tv/demo.php?id=1423&f=.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",NICK HD
+http://iptv2016-001-site1.1tempurl.com/live/jdm/315.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",NICK JR
+http://o12teamus.tk:8081/jdmsociety/nickjr/playlist.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",DISCOVERY KIDS
+http://videos.visionlatina.tv:1935/LiveATV/asd/playlist.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",DISCOVERY KIDS
+http://www.eliplextv.com/tv/demo.php?id=309&f=.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",Nicktoons
+http://www.eliplextv.com/tv/demo.php?id=3947&f=.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",TOONCAST
+http://www.eliplextv.com/tv/demo.php?id=443&f=.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",TIIN
+http://www.eliplextv.com/tv/demo.php?id=3947&f=.m3u8
+
+#EXTINF:-1 group-title="INFANTIL",CBEEBIES
+http://www.eliplextv.com/tv/demo.php?id=1029&f=.m3u8
+
+#EXTINF:-1 group-title="MUSICA",BANDAMAX
+http://www.eliplextv.com/tv/demo.php?id=1467&f=.m3u8
+
+#EXTINF:-1 group-title="MUSICA",MTV 
+http://iptv2016-001-site1.1tempurl.com/live/jdm/726.m3u8
+
+#EXTINF:-1 group-title="MUSICA",MTV Hit
+http://iptv2016-001-site1.1tempurl.com/live/jdm/728.m3u8
+
+#EXTINF:-1 group-title="MUSICA",MTV Live
+http://iptv2016-001-site1.1tempurl.com/live/jdm/701.m3u8
+
+#EXTINF:-1 group-title="MUSICA",RITMOSON
+http://iptv2016-001-site1.1tempurl.com/live/jdm/731.m3u8
+
+#EXTINF:-1 group-title="MUSICA",TELEHIT HD
+http://iptv2016-001-site1.1tempurl.com/live/jdm/702.m3u8
+
+#EXTINF:-1 group-title="MUSICA",VH1 
+http://iptv2016-001-site1.1tempurl.com/live/jdm/703.m3u8
+
+#EXTINF:-1 group-title="MUSICA",VH1 classic
+http://iptv2016-001-site1.1tempurl.com/live/jdm/728.m3u8
+
+#EXTINF:-1 group-title="MUSICA",ULTRA FIESTA
+http://vemoxhlslive-i.akamaihd.net/hls/live/219941/vemoxchannel2/iOS/vemoxchannel2@219941/index.m3u8
+
+#EXTINF:-1,TIGO MUSIC
+http://hlslive.lcdn.une.net.co/v1/AUTH_HLSLIVE/MESH/tu1_1.m3u8?
+
+#EXTINF:-1,Teleritmo
+http://brightcove03-f.akamaihd.net/multimedios_tv_612k@55732?file=ChelTV_m3u8
+
+#EXTINF:-1 group-title="MUSICA",telemusica
+http://186.155.200.118:1935/live/telemusica/.m3u8
+
+#EXTINF:-1 group-title="MUSICA",Televitrola
+http://186.155.200.118:1935/live/Televitrola/.m3u8
+
+#EXTINF:-1 group-title="MUSICA",Sii Television
+http://iphone-streaming.ustream.tv/ustreamVideo/12096274/streams/live/playlist.m3u8
+
+#EXTINF:-1 group-title="MUSICA",Heavy Metal HD
+http://70.166.98.130:1935/hmtv/myStream/playlist.m3u8
+
+#EXTINF:-1 group-title="MUSICA",MIMUSICA
+http://hlslive.lcdn.une.net.co/v1/AUTH_HLSLIVE/MMHD/tu1_1.m3u8?
+
+#EXTINF:-1 group-title="MUSICA",Vevo Tv 1
+http://vevoplaylist-live.hls.adaptive.level3.net/vevo/ch1/05/prog_index.m3u8
+ 
+#EXTINF:-1 group-title="MUSICA",Vevo Tv 2
+http://vevoplaylist-live.hls.adaptive.level3.net/vevo/ch2/05/prog_index.m3u8
+ 
+#EXTINF:-1 group-title="MUSICA",Vevo Tv 3
+http://vevoplaylist-live.hls.adaptive.level3.net/vevo/ch3/06/prog_index.m3u8 
+
+#EXTINF:-1,Efekto TV
+http://grupomac-lh.akamaihd.net:80/i/004rss0k8_1@301782/master.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",Az noticias
+http://urtmpkal-f.akamaihd.net/i/077zoqxzy_1@130091/index_3_av-p.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",Foro TV
+http://185417-f.akamaihd.net/i/n0tf0r0_1@81283/index_1200_av-p.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",MILENIO HD
+http://brightcove03-f.akamaihd.net/milenio_centro_512k@101956/?file=cheltv_m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",24 Horas
+http://iphonelive.rtve.es/24H_LV3_IPH/bitrate_2.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",PlayBoy
+http://s2.edem.tv/iptv/M3MKGZW6NMMUGQ/527/index.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",BBC INGLES
+http://uneapple-i.akamaihd.net/hls/live/205014/grupoune_st5@205014/st5_index.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",DW Latinoamérica
+http://www.metafilegenerator.de/DWelle/tv-latinoamerica/ios/master.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",RT Noticias
+http://rt-a.akamaihd.net/ch_02@325606/720p.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",CCTV News
+http://wpc.c1a9.edgecastcdn.net/hls-live/20C1A9/cctv_news/ls_satlink/b_828.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",BBC WORLD
+http://wpc.c1a9.edgecastcdn.net/hls-live/20C1A9/bbc_world/ls_satlink/b_828.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",ABC News 
+http://abclive.abcnews.com/i/abc_live4@136330/index_1200_av-b.m3u8?sd=10&b=1200&rebase=on
+
+#EXTINF:-1 group-title="NOTICIAS",CNN 
+http://wpc.c1a9.edgecastcdn.net/hls-live/20C1A9/cnn/ls_satlink/b_828.m3u8
+
+#EXTINF:-1 group-title="NOTICIAS",Deutsche Welle
+http://dwtvios_asiaplus-i.akamaihd.net/hls/live/200521-b/dwtvasiaplus/x1x/playlist1x.m3u8
+
+#EXTINF:-1, CB24
+rtmp://ipointcablertmp.tulix.tv:1935/cb24stream/myStream.sdp
+
+#EXTINF:-1, Euronews en Español
+http://fr-par-iphone-1.cdn.hexaglobe.net/streaming/euronews_ewns/ipad_es.m3u8
+
